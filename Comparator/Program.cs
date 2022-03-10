34c58141520e10e2ed690e2516d17b7b09960139
jiriv <jiriv@DESKTOP-NK4T1DB>
2022-03-10 10:31:22 +0100

    Přidejte soubory projektu.

---
+++ Comparator/Program.cs
@@ -0,0 +1,42 @@
+﻿while (true)
+{
+    // První číslo
+    Console.Write("Zadejte první číslo: ");
+    int c1 = Convert.ToInt32(Console.ReadLine());
+
+    // Druhé číslo
+    Console.Write("Zadejte druhé číslo: ");
+    int c2 = Convert.ToInt32(Console.ReadLine());
+
+    // Vyvolání metody Comparator
+    Comparator(c1, c2);
+
+    Console.WriteLine("Pro pokračování stiskněte y");
+    string y = Console.ReadLine();
+    if (y == "y")
+    {
+        Console.Clear();
+        continue;
+    }
+    else
+    {
+        break;
+    }
+}
+
+// Metoda pro porovnávání čísel
+void Comparator(int a, int b)
+{
+    if (a > b)
+    {
+        Console.WriteLine($"Číslo {a} je větší než {b}");
+        Console.ReadKey();
+        Console.Clear();
+    }
+    else if (a < b)
+    {
+        Console.WriteLine($"Číslo {a} je menší než {b}");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
